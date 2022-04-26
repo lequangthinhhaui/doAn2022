386b56e1038df0f0e2e9ac407cefdf13f1655471
Quang Thịnh Lê <lequangthinhhaui@gmail.com>
2022-04-27 01:58:15 +0700

    add code to bui thi hong 22018605226

--- Bui_Quoc_Tuan_2018605300/CODE/synButtonMqtt/synButtonMqtt.ino
+++ Bui_Quoc_Tuan_2018605300/CODE/synButtonMqtt/synButtonMqtt.ino
@@ -26,11 +26,11 @@
 
 //define mqtt
 #define mqtt_server "broker.hivemq.com"
-#define mqtt_user ""    
+#define mqtt_user ""
 #define mqtt_pwd ""
-const uint16_t mqtt_port = 1883; 
+const uint16_t mqtt_port = 1883;
 
-String mqtt_topic_pub = "buiquoctuan/btncontrol";   
+String mqtt_topic_pub = "buiquoctuan/btncontrol";
 String mqtt_topic_sub = "buiquoctuan/btnrespon";
 
 //user variable
@@ -46,7 +46,7 @@ float nhietdo = 0;
 
 unsigned long last = 0, bien = 0;
 
-//define 
+//define
 int button1PressCount = 0;
 int button2PressCount = 0;
 int button3PressCount = 0;
@@ -107,7 +107,7 @@ void handleMQTT(void *parameter);
 
 void setup() {
   Serial.begin(115200);
-  client.setServer(mqtt_server, mqtt_port); 
+  client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
   pinInit(6, 6);
   taskInit();
@@ -123,11 +123,11 @@ void loop() {
 
 void pinInit(int pinNumberInput, int pinNumberOnput)
 {
-  for(int i = 0; i < pinNumberInput; i ++)
+  for (int i = 0; i < pinNumberInput; i ++)
   {
     pinMode(inputPinArr[i], INPUT);
   }
-  for(int i = 0; i < pinNumberOnput; i ++)
+  for (int i = 0; i < pinNumberOnput; i ++)
   {
     pinMode(outputPinArr[i], OUTPUT);
   }
@@ -139,110 +139,110 @@ void buttonInit()
 }
 void taskInit()
 {
-  handleButton1Task(); 
+  handleButton1Task();
   handleMQTTTask();
 }
 void handleButton1Task()
 {
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              handleButton1,  // Function to be called
-              "handleButton1",   // Name of task
-              2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              1);            
+    handleButton1,  // Function to be called
+    "handleButton1",   // Name of task
+    2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,         // Task handle
+    1);
 }
 
 void handleButton2Task()
 {
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              handleButton2,  // Function to be called
-              "handleButton2",   // Name of task
-              2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              1);            
+    handleButton2,  // Function to be called
+    "handleButton2",   // Name of task
+    2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,         // Task handle
+    1);
 }
 
 void handleButton3Task()
 {
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              handleButton3,  // Function to be called
-              "handleButton3",   // Name of task
-              2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              1);            
+    handleButton3,  // Function to be called
+    "handleButton3",   // Name of task
+    2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,         // Task handle
+    1);
 }
 
 void handleButto4Task()
 {
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              handleButton4,  // Function to be called
-              "handleButton4",   // Name of task
-              2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              1);            
+    handleButton4,  // Function to be called
+    "handleButton4",   // Name of task
+    2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,         // Task handle
+    1);
 }
 
 void handleButton5Task()
 {
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              handleButton5,  // Function to be called
-              "handleButton5",   // Name of task
-              2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              1);            
+    handleButton5,  // Function to be called
+    "handleButton5",   // Name of task
+    2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,         // Task handle
+    1);
 }
 
 
 void handleButton6Task()
 {
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              handleButton6,  // Function to be called
-              "handleButton6",   // Name of task
-              2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              1);            
+    handleButton6,  // Function to be called
+    "handleButton6",   // Name of task
+    2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,         // Task handle
+    1);
 }
 
 
 void handleMQTTTask()
 {
   xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              handleMQTT,  // Function to be called
-              "handleMQTT",   // Name of task
-              2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              1);            
+    handleMQTT,  // Function to be called
+    "handleMQTT",   // Name of task
+    2048,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,         // Task handle
+    1);
 }
 
 void handleButton1(void *parameter) {
-  while(1) {
-    while(digitalRead(BTN_01) == HIGH);
-    while(digitalRead(BTN_01) == LOW);
-    button1PressCount ++;   
-    if(button1PressCount == 2)
+  while (1) {
+    while (digitalRead(BTN_01) == HIGH);
+    while (digitalRead(BTN_01) == LOW);
+    button1PressCount ++;
+    if (button1PressCount == 2)
     {
       button1PressCount = 0;
     }
-    if(button1PressCount == 0)
+    if (button1PressCount == 0)
     {
       digitalWrite(OUT_01, HIGH);
       client.publish(mqtt_topic_pub.c_str(), "11");
     }
-    else if(button1PressCount == 1)
+    else if (button1PressCount == 1)
     {
       digitalWrite(OUT_01, LOW);
       client.publish(mqtt_topic_pub.c_str(), "10");
@@ -253,20 +253,20 @@ void handleButton1(void *parameter) {
 //handle button 2
 
 void handleButton2(void *parameter) {
-  while(1) {
-    while(digitalRead(BTN_02) == HIGH);
-    while(digitalRead(BTN_02) == LOW);
-    button2PressCount ++;   
-    if(button2PressCount == 2)
+  while (1) {
+    while (digitalRead(BTN_02) == HIGH);
+    while (digitalRead(BTN_02) == LOW);
+    button2PressCount ++;
+    if (button2PressCount == 2)
     {
       button2PressCount = 0;
     }
-    if(button2PressCount == 0)
+    if (button2PressCount == 0)
     {
       digitalWrite(OUT_02, HIGH);
       client.publish(mqtt_topic_pub.c_str(), "21");
     }
-    else if(button2PressCount == 1)
+    else if (button2PressCount == 1)
     {
       digitalWrite(OUT_02, LOW);
       client.publish(mqtt_topic_pub.c_str(), "20");
@@ -278,20 +278,20 @@ void handleButton2(void *parameter) {
 //handle button 3
 
 void handleButton3(void *parameter) {
-  while(1) {
-    while(digitalRead(BTN_03) == HIGH);
-    while(digitalRead(BTN_03) == LOW);
-    button3PressCount ++;   
-    if(button3PressCount == 2)
+  while (1) {
+    while (digitalRead(BTN_03) == HIGH);
+    while (digitalRead(BTN_03) == LOW);
+    button3PressCount ++;
+    if (button3PressCount == 2)
     {
       button3PressCount = 0;
     }
-    if(button3PressCount == 0)
+    if (button3PressCount == 0)
     {
       digitalWrite(OUT_03, HIGH);
       client.publish(mqtt_topic_pub.c_str(), "31");
     }
-    else if(button3PressCount == 1)
+    else if (button3PressCount == 1)
     {
       digitalWrite(OUT_03, LOW);
       client.publish(mqtt_topic_pub.c_str(), "30");
@@ -300,20 +300,20 @@ void handleButton3(void *parameter) {
 }
 
 void handleButton4(void *parameter) {
-  while(1) {
-    while(digitalRead(BTN_04) == HIGH);
-    while(digitalRead(BTN_04) == LOW);
-    button4PressCount ++;   
-    if(button4PressCount == 2)
+  while (1) {
+    while (digitalRead(BTN_04) == HIGH);
+    while (digitalRead(BTN_04) == LOW);
+    button4PressCount ++;
+    if (button4PressCount == 2)
     {
       button4PressCount = 0;
     }
-    if(button4PressCount == 0)
+    if (button4PressCount == 0)
     {
       digitalWrite(OUT_04, HIGH);
       client.publish(mqtt_topic_pub.c_str(), "41");
     }
-    else if(button4PressCount == 1)
+    else if (button4PressCount == 1)
     {
       digitalWrite(OUT_04, LOW);
       client.publish(mqtt_topic_pub.c_str(), "40");
@@ -323,20 +323,20 @@ void handleButton4(void *parameter) {
 
 
 void handleButton5(void *parameter) {
-  while(1) {
-    while(digitalRead(BTN_05) == HIGH);
-    while(digitalRead(BTN_05) == LOW);
-    button5PressCount ++;   
-    if(button5PressCount == 2)
+  while (1) {
+    while (digitalRead(BTN_05) == HIGH);
+    while (digitalRead(BTN_05) == LOW);
+    button5PressCount ++;
+    if (button5PressCount == 2)
     {
       button5PressCount = 0;
     }
-    if(button5PressCount == 0)
+    if (button5PressCount == 0)
     {
       digitalWrite(OUT_05, HIGH);
       client.publish(mqtt_topic_pub.c_str(), "51");
     }
-    else if(button5PressCount == 1)
+    else if (button5PressCount == 1)
     {
       digitalWrite(OUT_05, LOW);
       client.publish(mqtt_topic_pub.c_str(), "50");
@@ -345,20 +345,20 @@ void handleButton5(void *parameter) {
 }
 
 void handleButton6(void *parameter) {
-  while(1) {
-    while(digitalRead(BTN_06) == HIGH);
-    while(digitalRead(BTN_06) == LOW);
-    button6PressCount ++;   
-    if(button6PressCount == 2)
+  while (1) {
+    while (digitalRead(BTN_06) == HIGH);
+    while (digitalRead(BTN_06) == LOW);
+    button6PressCount ++;
+    if (button6PressCount == 2)
     {
       button6PressCount = 0;
     }
-    if(button6PressCount == 0)
+    if (button6PressCount == 0)
     {
       digitalWrite(OUT_06, HIGH);
       client.publish(mqtt_topic_pub.c_str(), "61");
     }
-    else if(button6PressCount == 1)
+    else if (button6PressCount == 1)
     {
       digitalWrite(OUT_06, LOW);
       client.publish(mqtt_topic_pub.c_str(), "60");
@@ -369,7 +369,7 @@ void handleButton6(void *parameter) {
 
 
 void handleMQTT(void *parameter) {
-  while(1)
+  while (1)
   {
 
   }
@@ -385,7 +385,7 @@ void reconnect()
 
   while (!client.connected())
   {
-    String clientId = String(random(0xffff), HEX); 
+    String clientId = String(random(0xffff), HEX);
     if (client.connect(clientId.c_str(), mqtt_user, mqtt_pwd))
     {
       Serial.println("Connected MQTT ngoinhaiot.com");
@@ -412,67 +412,67 @@ void callback(char* topic, byte* payload, unsigned int length)
   }
   dataInt = Data.toInt();
   Serial.println(dataInt);
-    ///button 1
-  if(dataInt == 11)
+  ///button 1
+  if (dataInt == 11)
   {
     button1PressCount = 1;
     digitalWrite(OUT_01, HIGH);
     client.publish(mqtt_topic_pub.c_str(), "11");
   }
-  else if(dataInt == 10)
+  else if (dataInt == 10)
   {
     button1PressCount = 0;
     digitalWrite(OUT_01, LOW);
     client.publish(mqtt_topic_pub.c_str(), "10");
   }
-    ///button 2
-  else if(dataInt == 21)
+  ///button 2
+  else if (dataInt == 21)
   {
     button2PressCount = 1;
     digitalWrite(OUT_02, HIGH);
     client.publish(mqtt_topic_pub.c_str(), "21");
   }
-  else if(dataInt == 20)
+  else if (dataInt == 20)
   {
     button2PressCount = 0;
     digitalWrite(OUT_02, LOW);
     client.publish(mqtt_topic_pub.c_str(), "20");
   }
-    ///button 3
-  else if(dataInt == 31)
+  ///button 3
+  else if (dataInt == 31)
   {
     button2PressCount = 1;
     digitalWrite(OUT_03, HIGH);
     client.publish(mqtt_topic_pub.c_str(), "31");
   }
-  else if(dataInt == 30)
+  else if (dataInt == 30)
   {
     button3PressCount = 0;
     digitalWrite(OUT_03, LOW);
     client.publish(mqtt_topic_pub.c_str(), "30");
   }
 
-    ///button 4
-  else if(dataInt == 41)
+  ///button 4
+  else if (dataInt == 41)
   {
     button4PressCount = 1;
     digitalWrite(OUT_04, HIGH);
     client.publish(mqtt_topic_pub.c_str(), "41");
   }
-  else if(dataInt == 40)
+  else if (dataInt == 40)
   {
     button4PressCount = 0;
     digitalWrite(OUT_04, LOW);
     client.publish(mqtt_topic_pub.c_str(), "40");
   }
-    ///button 5
-  else if(dataInt == 51)
+  ///button 5
+  else if (dataInt == 51)
   {
     button5PressCount = 1;
     digitalWrite(OUT_05, HIGH);
     client.publish(mqtt_topic_pub.c_str(), "51");
   }
-  else if(dataInt == 50)
+  else if (dataInt == 50)
   {
     button5PressCount = 0;
     digitalWrite(OUT_05, LOW);
@@ -480,13 +480,13 @@ void callback(char* topic, byte* payload, unsigned int length)
   }
 
   ///button 6
-  else if(dataInt == 61)
+  else if (dataInt == 61)
   {
     button6PressCount = 1;
     digitalWrite(OUT_06, HIGH);
     client.publish(mqtt_topic_pub.c_str(), "61");
   }
-  else if(dataInt == 60)
+  else if (dataInt == 60)
   {
     button6PressCount = 0;
     digitalWrite(OUT_06, LOW);
---
+++ Bui_Thi_Hong_2018605226/CODE/ESP32_TEMP_HUMI/demo/demo.ino
@@ -0,0 +1,123 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+#include <Wire.h>
+#include <DHT.h>
+#include <AsyncElegantOTA.h>
+#include <LiquidCrystal_I2C.h>
+
+#define ssid "Hong"
+#define password "hongdt4"
+
+//khai bao bien luu nhiet do va do am
+float temper = 0.0;
+float humi = 0.0;
+float temperPre = 0.0;
+float humiPre = 0.0;
+
+String temperStr = " ";
+String humiStr = " ";
+
+//init lcd
+LiquidCrystal_I2C lcd(0x27, 16, 2); //lcd i2c 16x2
+
+#define DHTPIN 15     // what pin we're connected to
+#define DHTTYPE DHT21  // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String readDHTTemp() 
+{
+  // Read temperature as Celsius (the default)
+  temper = dht.readTemperature();
+  if(temper > 0)
+  {
+    temperPre = temper;
+  }
+
+  if (isnan(temper)) {    
+    Serial.println("khong doc duoc nhiet do");
+    temper = temperPre;
+    return String(temper);
+  }
+  else {
+    Serial.println(temper);
+    return String(temper);
+  }
+}
+
+String readDHTHum() 
+{
+  humi = dht.readHumidity();
+  if(humi > 0)
+  {
+    humiPre = humi;
+  }
+  if (isnan(humi)) {
+    Serial.println("khong doc duoc do am");
+    humi = humiPre;
+    return String(humi);
+  }
+  else {
+    Serial.println(humi);
+    return String(humi);
+  }
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+  dht.begin();
+
+  //khoi tao lcd
+  lcd.init();
+  // bat led nen                     
+  lcd.backlight();
+
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  // Connect to Wi-Fi
+  WiFi.softAP(ssid, password);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html");
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", temperStr.c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", humiStr.c_str());
+  });
+  AsyncElegantOTA.begin(&server);
+  server.begin();
+}
+ 
+void loop(){
+  temperStr = readDHTTemp();
+  humiStr = readDHTHum();
+  //set con tro hang thu nhat, cot thu nhat
+  lcd.setCursor(0, 0);
+  //in ra man hinh
+  lcd.print("Temp: ");
+  lcd.setCursor(6, 0);
+  lcd.print(temper);
+  //set con tro hang thu hai, cot thu nhat
+  lcd.setCursor(0,1);
+  lcd.print("Humi: ");
+  lcd.setCursor(6, 1);
+  lcd.print(humi);
+
+  delay(5000);
+  
+}
